fc1217815966d7dd7d48c5eb3057c269e270e5c5
NicoAG12 <73404339+NicoAG12@users.noreply.github.com>
2023-05-17 21:43:39 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import {Calculadora,Calculadora2} from "./components"
 function App() {
   return (
   <div>
-    <Calculadora/>
-    <Calculadora2/>
+    hola mundo
   </div>
 
   );
