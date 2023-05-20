e0c940c3807143d0f99ab41e076178f9b8f3cf84
NicoAG12 <agueronico247@gmail.com>
2023-05-20 11:12:49 -0300

    Se hicieron correcciones en la rama NavBar
    Se eliminaron componentes no correspondientes a la rama

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import { NavPrincipal } from './Components/Navbar';
-import { CardProducto } from "./Components/CardProducto"
 import './App.css';
 function App() {
   return (
     <div>
       <NavPrincipal />
-      <CardProducto />
     </div>
 
   );
