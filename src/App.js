fe2ef27543e28e765e0a6edf8e234f5ee3dc6a59
NicoAG12 <agueronico247@gmail.com>
2023-05-22 17:23:26 -0300

    Se arreglo import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from "./logo.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Container from "react-bootstrap/Container";
-import { CardProductos } from "./components/CardProductos";
+import { CardProductos } from "./Components/CardProductos";
+
 function App() {
   return (
     <>
