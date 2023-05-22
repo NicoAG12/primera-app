6ed321b401046987625f63f048bc5ee535adb355
NicoAG12 <agueronico247@gmail.com>
2023-05-22 17:29:04 -0300

    Corrigio import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CarouselPrincipal } from "./components/Carousel";
-import { NavPrincipal } from "./components/Navbar";
-import { CardProductos } from "./components/CardProductos";
+import { CarouselPrincipal } from "./Components/Carousel";
+import { NavPrincipal } from "./Components/Navbar";
+import { CardProductos } from "./Components/CardProductos";
 
 function App() {
   return (
